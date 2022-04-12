ef913e5124356ad544e8231f15eb180fcc2fd3cb
hardy.rafael17@gmail.com <Hardy Jimenez Matos>
2022-04-12 12:12:13 -0400

    refactor: refactor server.c
    add personal configuration to server

--- server.c
+++ server.c
@@ -1,46 +1,33 @@
 #include <signal.h>
 #include <unistd.h>
-#include "libft/libft.h"
+#include <sys/types.h>
+#include <stdio.h>
 
-static void	action(int sig, siginfo_t *info, void *context)
+static void	action(int signo, siginfo_t *info, void *context)
 {
-	static int				i = 0;
-	static pid_t			client_pid = 0;
-	static unsigned char	c = 0;
-
-	(void)context;
-	if (!client_pid)
-		client_pid = info->si_pid;
-	c |= (sig == SIGUSR2);
-	if (++i == 8)
-	{
-		i = 0;
-		if (!c)
-		{
-			kill(client_pid, SIGUSR2);
-			client_pid = 0;
-			return ;
-		}
-		ft_putchar_fd(c, 1);
-		c = 0;
-		kill(client_pid, SIGUSR1);
-	}
-	else
-		c <<= 1;
+	printf("signal received was digit:%d\n", signo);
 }
 
 int	main(void)
 {
-	struct sigaction	s_sigaction;
+	int	stop;
+	pid_t	server_pid;
 
-	ft_putstr_fd("Server PID: ", 1);
-	ft_putnbr_fd(getpid(), 1);
-	ft_putchar_fd('\n', 1);
+	server_pid = getpid();
+	struct sigaction	s_sigaction;
+	stop = 10;
+
+	/*Print server pid */
+	printf("Server PID: %d\n", server_pid);
+
 	s_sigaction.sa_sigaction = action;
 	s_sigaction.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &s_sigaction, 0);
 	sigaction(SIGUSR2, &s_sigaction, 0);
-	while (1)
+	while (stop)
+	{
 		pause();
+		stop--;
+	}
 	return (0);
 }
