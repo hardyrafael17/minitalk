a197c67760d2bc7be8115dda5901407d3cb96c7b
Hardy Jimenez Matos <hardy.rafael17@gmail.com>
2022-04-13 00:07:04 +0200

    fix: made server and client work
    made client and server communicate

--- client.c
+++ client.c
@@ -1,60 +1,29 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "libft/libft.h"
-
-static void	action(int sig)
-{
-	static int	received = 0;
-
-	if (sig == SIGUSR1)
-		++received;
-	else
-	{
-		ft_putnbr_fd(received, 1);
-		ft_putchar_fd('\n', 1);
-		exit(0);
-	}
-}
-
-static void	mt_kill(int pid, char *str)
-{
-	int		i;
-	char	c;
-
-	while (*str)
-	{
-		i = 8;
-		c = *str++;
-		while (i--)
-		{
-			if (c >> i & 1)
-				kill(pid, SIGUSR2);
-			else
-				kill(pid, SIGUSR1);
-			usleep(100);
-		}
-	}
-	i = 8;
-	while (i--)
-	{
-		kill(pid, SIGUSR1);
-		usleep(100);
-	}
-}
+#include <string.h>
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
-	if (argc != 3 || !ft_strlen(argv[2]))
+	int	counter;
+
+	counter = 0;
+
+	if (argc != 3 || !strlen(argv[2]))
+	{
+		printf("Argument count %d\n", argc);
 		return (1);
-	ft_putstr_fd("Sent    : ", 1);
-	ft_putnbr_fd(ft_strlen(argv[2]), 1);
-	ft_putchar_fd('\n', 1);
-	ft_putstr_fd("Received: ", 1);
-	signal(SIGUSR1, action);
-	signal(SIGUSR2, action);
-	mt_kill(ft_atoi(argv[1]), argv[2]);
-	while (1)
-		pause();
+	}
+
+	printf("sending to pid: %d\n", atoi(argv[2]));
+	//mt_kill(ft_atoi(argv[1]), argv[2]);
+	while (counter <= 10000)
+	{
+		kill(atoi(argv[2]), SIGUSR1);
+		printf("sending %d\n", atoi(argv[2]));
+		counter++;
+	}
+	printf("Dene!\n");
 	return (0);
 }
--- server.c
+++ server.c
@@ -2,10 +2,13 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <string.h>
 
+int		g_counter = 0;
 static void	action(int signo, siginfo_t *info, void *context)
 {
-	printf("signal received was digit:%d\n", signo);
+	printf("Counter ==> %d\n", g_counter);
+	g_counter++;
 }
 
 int	main(void)
@@ -20,14 +23,14 @@ int	main(void)
 	/*Print server pid */
 	printf("Server PID: %d\n", server_pid);
 
-	s_sigaction.sa_sigaction = action;
+	s_sigaction.sa_sigaction = &action;
 	s_sigaction.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &s_sigaction, 0);
 	sigaction(SIGUSR2, &s_sigaction, 0);
 	while (stop)
 	{
+		printf("pausing\n");
 		pause();
-		stop--;
 	}
 	return (0);
 }
