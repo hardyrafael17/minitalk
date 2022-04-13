4b41c5d1abe4118e75e7ffdcaac40e4a0531463c
Hardy Jimenez Matos <hardy.rafael17@gmail.com>
2022-04-13 20:43:02 +0200

    feat: fixed server
    fixed server to handle 2 signals

--- server.c
+++ server.c
@@ -5,29 +5,43 @@
 #include <string.h>
 
 int		g_counter = 0;
-static void	action(int signo, siginfo_t *info, void *context)
+
+static
+void	action(int signo, siginfo_t *info, void *context)
 {
-	printf("Counter ==> %d\n", g_counter);
 	g_counter++;
 }
 
-int	main(void)
+static
+void	action2(int signo, siginfo_t *info, void *context)
+{
+	
+	printf("Counter ==> %d\n", g_counter);
+}
+
+int
+main(void)
 {
 	int	stop;
 	pid_t	server_pid;
 
 	server_pid = getpid();
 	struct sigaction	s_sigaction;
-	stop = 10;
+	struct sigaction	s_sigaction2;
+
 
 	/*Print server pid */
 	printf("Server PID: %d\n", server_pid);
 
 	s_sigaction.sa_sigaction = &action;
+	s_sigaction2.sa_sigaction = &action2;
+
 	s_sigaction.sa_flags = SA_SIGINFO;
+	s_sigaction2.sa_flags = SA_SIGINFO;
+	
 	sigaction(SIGUSR1, &s_sigaction, 0);
-	sigaction(SIGUSR2, &s_sigaction, 0);
-	while (stop)
+	sigaction(SIGUSR2, &s_sigaction2, 0);
+	while (1)
 	{
 		printf("pausing\n");
 		pause();
